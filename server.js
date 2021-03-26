c6d57487ecc875afa4a43f73b13e1410904072a7
tramnewin <trmnguyen2608@gmail.com>
2021-03-26 12:21:34 -0600

    second commit

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ var User = require('./Users');
 var Movie = require('./movies')
 var Review = require('./reviews')
 const crypto = require("crypto");
-var rp = require('request-promise');
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -41,38 +40,6 @@ function getJSONObjectForMovieRequirement(req) {
     }
 
     return json;
-}
-function trackDimension(category, action, label, value, dimension, metric) {
-
-    var options = { method: 'GET',
-        url: 'https://www.google-analytics.com/collect',
-        qs:
-            {   // API Version.
-                v: '1',
-                // Tracking ID / Property ID.
-                tid: GA_TRACKING_ID,
-                // Random Client Identifier. Ideally, this should be a UUID that
-                // is associated with particular user, device, or browser instance.
-                cid: crypto.randomBytes(16).toString("hex"),
-                // Event hit type.
-                t: 'event',
-                // Event category.
-                ec: category,
-                // Event action.
-                ea: action,
-                // Event label.
-                el: label,
-                // Event value.
-                ev: value,
-                // Custom Dimension
-                cd1: dimension,
-                // Custom Metric
-                cm1: metric
-            },
-        headers:
-            {  'Cache-Control': 'no-cache' } };
-
-    return rp(options);
 }
 
 router.post('/signup', function(req, res) {
